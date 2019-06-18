b4bc2840745155170c4ca387168666851a371d59
Diogo Ferreira <dferreira@hipay.com>
2019-06-18 12:25:30 +0100

    update notification

--- lib/HipayMbway/MbwayNotification.php
+++ lib/HipayMbway/MbwayNotification.php
@@ -1,13 +1,14 @@
 <?php
-namespace HipayMbway; 
+
+namespace HipayMbway;
 
 /**
  * Description of MbwayNotification
  *
  * @author hipay.pt
  */
-
 class MbwayNotification {
+
     private $OperationId;
     private $Amount;
     private $Phone;
@@ -17,15 +18,15 @@ class MbwayNotification {
     private $StatusDescriptionDetails;
     private $ClientExternalReference;
     private $isJson;
-            
-	function __construct($response) {
+
+    function __construct($response) {
         $responseObj = json_decode($response, false);
-        
+
         if (is_null($responseObj) || json_last_error() != JSON_ERROR_NONE) {
             $this->set_isJson(false);
             return false;
         }
-        
+
         $this->set_isJson(true);
         $this->set_OperationId($responseObj->OperationId);
         $this->set_Amount($responseObj->Amount);
@@ -36,58 +37,77 @@ class MbwayNotification {
         $this->set_StatusDescriptionDetails($responseObj->StatusDescriptionDetails);
         $this->set_ClientExternalReference($responseObj->ClientExternalReference);
     }
+
     function get_OperationId() {
         return $this->OperationId;
     }
+
     function get_Amount() {
         return $this->Amount;
     }
+
     function get_Phone() {
         return $this->Phone;
     }
+
     function get_Description() {
         return $this->Description;
     }
+
     function get_StatusCode() {
         return $this->StatusCode;
     }
+
     function get_StatusDescription() {
         return $this->StatusDescription;
     }
+
     function get_StatusDescriptionDetails() {
         return $this->StatusDescriptionDetails;
     }
+
     function get_ClientExternalReference() {
         return $this->ClientExternalReference;
     }
+
     function get_isJson() {
         return $this->isJson;
-    }    
+    }
+
     function set_Amount($Amount) {
         $this->Amount = $Amount;
     }
+
     function set_isJson($isJson) {
         $this->isJson = $isJson;
     }
+
     function set_OperationId($OperationId) {
         $this->OperationId = $OperationId;
     }
+
     function set_Phone($Phone) {
         $this->Phone = $Phone;
     }
+
     function set_Description($Description) {
         $this->Description = $Description;
     }
+
     function set_StatusCode($StatusCode) {
         $this->StatusCode = $StatusCode;
     }
+
     function set_StatusDescription($StatusDescription) {
         $this->StatusDescription = $StatusDescription;
     }
+
     function set_StatusDescriptionDetails($StatusDescriptionDetails) {
         $this->StatusDescriptionDetails = $StatusDescriptionDetails;
     }
+
     function set_ClientExternalReference($ClientExternalReference) {
         $this->ClientExternalReference = $ClientExternalReference;
     }
+
 }
